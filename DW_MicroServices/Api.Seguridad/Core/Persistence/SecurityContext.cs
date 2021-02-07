1821e65d515031a0a2f792180efce03f1e366de5
IQADMNT01\YoeC <yoe.cardenas@iq-online.com>
2021-02-07 11:56:29 -0500

    Se agrega Core al proyecto seguridad
    Se crea entidad usuario, contexto para manejo de usuarios en el api con entityframework y conexión a DB. Se crean scripts de DB para creación de objetos

---
+++ DW_MicroServices/Api.Seguridad/Core/Persistence/SecurityContext.cs
@@ -0,0 +1,20 @@
+﻿using Api.Seguridad.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Seguridad.Core.Persistence
+{
+    public class SecurityContext : IdentityDbContext<User>
+    {
+        public SecurityContext(DbContextOptions opt) : base(opt) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- DW_MicroServices/Api.Seguridad/Startup.cs
+++ DW_MicroServices/Api.Seguridad/Startup.cs
@@ -1,6 +1,8 @@
+using Api.Seguridad.Core.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +27,10 @@ namespace Api.Seguridad
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<SecurityContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("ConexionDB"));
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
